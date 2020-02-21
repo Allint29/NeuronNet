a1dbf58d229c69abc76cd1e8d0fa598d473309e1
Alexey Aniskin <AlexeyA29@yandex.ru>
2020-02-21 21:30:33 +0300

    Добавьте файлы проекта.

---
+++ NeuronNetwork/Models/Neuron.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NeuronNetwork.Enums;
+
+namespace NeuronNetwork.Models
+{
+    public class Neuron
+    {
+        public List<double> Weights { get; }
+        public NeuronType NeuronType { get; }
+        public double Output { get; private set; }
+
+        public Neuron(int inputCount, NeuronType type = NeuronType.Normal)
+        {
+            NeuronType = type;
+            Weights = new List<double>();
+
+            for (int i = 0; i < inputCount; i++)
+            {
+                Weights.Add(1.0);
+            }
+        }
+
+        public double FeedForward(List<double> inputs)
+        {
+            var sum = 0.0;
+            for (int i = 0; i < inputs.Count; i++)
+            {
+                sum += inputs[i] * Weights[i];
+            }
+
+            Output = Sigmoid(sum);
+            return Output;
+
+        }
+
+        public double Sigmoid(double x) => 1.0 / (1.0 + Math.Pow(Math.E, -x));
+
+        public override string ToString() => Output.ToString();
+    }
+}
